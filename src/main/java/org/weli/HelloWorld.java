26d189e023da48a6a615aee37aa752e9748a2d7f
Weinan Li <l.weinan@gmail.com>
2016-06-18 10:44:51 +0800

    add files

---
+++ src/main/java/org/weli/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.weli;
+
+/**
+ * Created by weli on 6/17/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
